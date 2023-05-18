e0eddfcf9ae5255aecd904b86f1a5f89e98ec58d
Raul Assia <raul.assia@well.co>
2023-05-18 13:15:13 -0500

    Initial Commit

---
+++ catsApp/catsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  catsAppApp.swift
+//  catsApp
+//
+//  Created by Raul Mantilla on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct catsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
